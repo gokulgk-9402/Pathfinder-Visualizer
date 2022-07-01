f64f700a1526b5c10707b4c8074770d762a2d88d
gokulgk-9402 <gokulgk9402@gmail.com>
2022-07-01 19:45:43 +0530

    Start, End choosing
    Start, End choosing in pygame window itself.
    Added Reset functionality.

--- main.py
+++ main.py
@@ -56,89 +56,120 @@ for i in range(COLUMNS):
     for j in range(ROWS):
         grid[i][j].set_neighbours()
 
-r, c = [int(e) for e in input("Enter start coordinates: ").split()]
-
-start_box = grid[r][c]
-start_box.isstart = True
-start_box.visited = True
-queue.append(start_box)
-
-def main():
-
-    begin_search = False
-    target_set = False
-    searching = True
-    target_b = None
+choosing_start = False
+choosing_end = False
+begin_search = False
+target_set = False
+start_set = False
+searching = True
+target_b = None
 
 
-    while True:
-        for event in pygame.event.get():
+while True:
+    for event in pygame.event.get():
 
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        
+        elif event.type == pygame.MOUSEBUTTONDOWN and choosing_start:
+            choosing_start = False
+            x = pygame.mouse.get_pos()[0]
+            y = pygame.mouse.get_pos()[1]
+            i = x // BOX_WIDTH
+            j = y // BOX_HEIGHT
+            start_box = grid[i][j]
+            start_box.isstart = True
+            start_box.visited = True
+            queue.append(start_box)
+            start_set = True
+        
+        elif event.type == pygame.MOUSEBUTTONDOWN and choosing_end:
+            choosing_end = False
+            x = pygame.mouse.get_pos()[0]
+            y = pygame.mouse.get_pos()[1]
+            i = x // BOX_WIDTH
+            j = y // BOX_HEIGHT
+            target = grid[i][j]
+            target.isend = True
+            target_set = True
+            target_set = True
 
-            elif event.type == pygame.MOUSEMOTION:
-                x = pygame.mouse.get_pos()[0]
-                y = pygame.mouse.get_pos()[1]
+        elif event.type == pygame.MOUSEMOTION and target_set and start_set:
+            x = pygame.mouse.get_pos()[0]
+            y = pygame.mouse.get_pos()[1]
 
-                if event.buttons[0]:
-                    i = x // BOX_WIDTH
-                    j = y // BOX_HEIGHT
-                    grid[i][j].iswall = True
-                
-                if event.buttons[2] and not target_set:
-                    i = x // BOX_WIDTH
-                    j = y // BOX_HEIGHT
-                    target = grid[i][j]
-                    target.isend = True
-                    target_set = True
+            if event.buttons[0]:
+                i = x // BOX_WIDTH
+                j = y // BOX_HEIGHT
+                grid[i][j].iswall = True
 
-            if event.type == pygame.KEYDOWN and target_set:
-                begin_search = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_s:
+                choosing_start = True
+            elif event.key == pygame.K_e:
+                choosing_end = True
+            elif event.key == pygame.K_SPACE:
+                if target_set and start_set:
+                    begin_search = True
+            elif event.key == pygame.K_r:
+                choosing_start = False
+                choosing_end = False
+                begin_search = False
+                target_set = False
+                start_set = False
+                searching = True
+                target_b = None
+                for row in grid:
+                    for box in row:
+                        box.isstart = False
+                        box.isend = False
+                        box.iswall = False
+                        box.queued = False
+                        box.visited = False
+                        box.prior = None
+                queue = []
+                path = []
 
-        if begin_search:
-            if len(queue) > 0 and searching:
-                current = queue.pop(0)
-                current.visited = True
-                if current == target:
-                    searching = False
-                    while current.prior != start_box:
-                        path.append(current.prior)
-                        current = current.prior
-                else:
-                    for neighbour in current.neighbours:
-                        if not neighbour.queued and not neighbour.iswall:
-                            neighbour.queued = True
-                            queue.append(neighbour)
-                            neighbour.prior = current
-
+    if begin_search:
+        if len(queue) > 0 and searching:
+            current = queue.pop(0)
+            current.visited = True
+            if current == target:
+                searching = False
+                while current.prior != start_box:
+                    path.append(current.prior)
+                    current = current.prior
             else:
-                if searching:
-                    Tk().wm_withdraw()
-                    messagebox.showinfo("No soultion", "There is no solution!")
-                    searching = False
+                for neighbour in current.neighbours:
+                    if not neighbour.queued and not neighbour.iswall:
+                        neighbour.queued = True
+                        queue.append(neighbour)
+                        neighbour.prior = current
 
-        window.fill((60, 60, 60))
+        else:
+            if searching:
+                Tk().wm_withdraw()
+                messagebox.showinfo("No soultion", "There is no solution!")
+                searching = False
 
-        for i in range(COLUMNS):
-            for j in range(ROWS):
-                if grid[i][j].isstart:
-                    grid[i][j].draw(window, (0, 150, 200))
-                elif grid[i][j].isend:
-                    grid[i][j].draw(window, (0, 200, 100))
-                elif grid[i][j].iswall:
-                    grid[i][j].draw(window, (0, 0, 0))
-                elif grid[i][j] in path:
-                    grid[i][j].draw(window, (100, 200, 100))
-                elif grid[i][j].visited:
-                    grid[i][j].draw(window, (250, 250, 150))
-                elif grid[i][j].queued:
-                    grid[i][j].draw(window, (250, 150, 150))
-                else: 
-                    grid[i][j].draw(window, (30, 30, 30))
+    window.fill((60, 60, 60))
 
+    for i in range(COLUMNS):
+        for j in range(ROWS):
+            if grid[i][j].isstart:
+                grid[i][j].draw(window, (0, 150, 200))
+            elif grid[i][j].iswall:
+                grid[i][j].draw(window, (0, 0, 0))
+            elif grid[i][j].isend:
+                grid[i][j].draw(window, (20, 250, 20))
+            elif grid[i][j] in path:
+                grid[i][j].draw(window, (100, 200, 100))
+            elif grid[i][j].visited:
+                grid[i][j].draw(window, (250, 250, 150))
+            elif grid[i][j].queued:
+                grid[i][j].draw(window, (250, 150, 150))
+            else: 
+                grid[i][j].draw(window, (30, 30, 30))
 
-        pygame.display.flip()
-
-main()
+    pygame.display.flip()
